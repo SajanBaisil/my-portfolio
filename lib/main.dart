a73c1a759ba3c3fdcb49af2ee5242bd9c15d7398
sajanIocod <sajan@iocod.com>
2024-10-10 11:19:16 +0530

    feat : added a browsing tab icon and changed browsing tag name

--- lib/main.dart
+++ lib/main.dart
@@ -32,7 +32,7 @@ class MyApp extends StatelessWidget {
         ),
       ],
       child: MaterialApp(
-          title: 'Flutter Demo',
+          title: 'Sajan Baisil Portfolio',
           theme: getApplicationThemeLight(context),
           home: const MainScreen()),
     );
